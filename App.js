cdb57b45bfe9b51d3e3a5d3ba782d2c71e08dd7f
Guilherme Shibuya <gyudishibuya@gmail.com>
2023-09-12 21:37:03 -0300

    começando testes com a camera

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Camera, CameraType } from "expo-camera";
 
 export default function App() {
+  const [type, setType] = useState();
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  useEffect(() => {
+    async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      requestPermission(status === "granted");
+    };
+  }, []);
+
+  const toggleCameraType = () => {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera 
+        style={styles.camera} 
+        type={type} 
+        useCamera2Api={true}
+      >
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  camera: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-end",
+    backgroundColor: "transparent",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#fff",
+    width: 80,
+    height: 80,
+    borderRadius: 100,
   },
 });
