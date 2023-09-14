70446be53482b909309dc6b8ea1c1976c55b4287
Guilherme Shibuya <gyudishibuya@gmail.com>
2023-09-14 08:04:25 -0300

    proporçao do view da camera ajustada

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import { StatusBar } from "expo-status-bar";
+import { Dimensions, StatusBar } from "react-native";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { useEffect, useState } from "react";
 import { Camera, CameraType } from "expo-camera";
+
+const WINDOW_WIDTH = Dimensions.get("window").width;
 
 export default function App() {
   const [type, setType] = useState();
@@ -14,31 +16,47 @@ export default function App() {
     };
   }, []);
 
-  const toggleCameraType = () => {
-    setType((current) =>
-      current === CameraType.back ? CameraType.front : CameraType.back
-    );
+  const takePicture = () => {
+    
   };
+
+  // const toggleCameraType = () => {
+  //   setType((current) =>
+  //     current === CameraType.back ? CameraType.front : CameraType.back
+  //   );
+  // };
+
+  if (!permission) {
+    return <Text>Permissão para a câmera negada</Text>;
+  }
 
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type} useCamera2Api={true}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
-            <Text>Flip Camera</Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
+      <View style={styles.header}></View>
+      <Camera
+        style={styles.camera}
+        type={CameraType.back}
+        useCamera2Api={true}
+      ></Camera>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}></TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   camera: {
+    width: WINDOW_WIDTH,
+    height: WINDOW_WIDTH * (4 / 3),
+  },
+  header: {
     flex: 1,
+    backgroundColor: "#000",
   },
   container: {
     flex: 1,
+    justifyContent: "center",
   },
   buttonContainer: {
     flex: 1,
@@ -46,11 +64,15 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     backgroundColor: "transparent",
     alignItems: "center",
+    backgroundColor: "#000",
   },
   button: {
     backgroundColor: "#fff",
     width: 80,
     height: 80,
     borderRadius: 100,
+    borderColor: "#dedede",
+    borderWidth: 8,
+    marginBottom: 24,
   },
 });
