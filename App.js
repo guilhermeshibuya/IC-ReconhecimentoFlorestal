162b1b979239d5ed44e64a41fc4d4089a97a2091
Guilherme Yudi Shibuya <gyudishibuya@gmail.com>
2023-09-14 12:52:50 -0300

    adicionada a função de salvar fotos na galeria

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
-import { Dimensions, StatusBar } from "react-native";
+import { Alert, Dimensions, StatusBar } from "react-native";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Camera, CameraType } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
 
 const WINDOW_WIDTH = Dimensions.get("window").width;
 
 export default function App() {
-  const [type, setType] = useState();
-  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const cameraRef = useRef(null);
+  const [isCameraReady, setIsCameraReady] = useState(false);
+  const [cameraPermission, requestCameraPermission] =
+    Camera.useCameraPermissions();
+  const [mediaPermission, requestMediaPermission] =
+    MediaLibrary.usePermissions();
 
   useEffect(() => {
-    async () => {
+    (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
-      requestPermission(status === "granted");
-    };
+      requestCameraPermission(status === "granted");
+    })();
+
+    (async () => {
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      requestMediaPermission(status === "granted");
+    })();
   }, []);
 
-  const takePicture = () => {
-    
+  const onCameraReady = () => {
+    setIsCameraReady(true);
+  };
+
+  const takePicture = async () => {
+    if (cameraRef.current && isCameraReady) {
+      const { uri } = await cameraRef.current.takePictureAsync();
+      const asset = await MediaLibrary.createAssetAsync(uri);
+    } else {
+      Alert.alert(
+        "Erro!",
+        "Não foi possível tirar a foto, camera não iniciada",
+        [{ text: "Ok" }]
+      );
+    }
   };
 
   // const toggleCameraType = () => {
@@ -26,8 +49,12 @@ export default function App() {
   //   );
   // };
 
-  if (!permission) {
+  if (!cameraPermission) {
     return <Text>Permissão para a câmera negada</Text>;
+  }
+
+  if (cameraPermission === null) {
+    return <View></View>;
   }
 
   return (
@@ -37,9 +64,14 @@ export default function App() {
         style={styles.camera}
         type={CameraType.back}
         useCamera2Api={true}
+        onCameraReady={onCameraReady}
+        ref={cameraRef}
       ></Camera>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}></TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={takePicture}
+        ></TouchableOpacity>
       </View>
     </View>
   );
@@ -61,8 +93,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flex: 1,
     flexDirection: "column",
-    justifyContent: "flex-end",
-    backgroundColor: "transparent",
+    justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#000",
   },
